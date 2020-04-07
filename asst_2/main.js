392463e27a9770b1a48d05048831046bf93a053c
Oluwatobi Ajide <biajide@gmail.com>
2020-04-06 22:47:56 -0400

    Update main.js

--- asst_2/main.js
+++ asst_2/main.js
@@ -11,7 +11,7 @@ for(let i = 1; i <= 5; i++) {
     newImage.setAttribute('src', 'images/pic' + i + '.jpg');
     thumbBar.appendChild(newImage);
     newImage.onclick = function(e) {
-      displayedImage.src = e.target.src;
+      displayedImage.src = e.target.src
     }
   }
 
